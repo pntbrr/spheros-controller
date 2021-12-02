70844392c12d82af16d2e45596df440004f80b53
Maëva <maeva@nother.io>
2021-12-02 17:15:13 +0100

    Initial commit🎉

---
+++ GrapesApp/SocketIOManager.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  PointBarre
+//
+//  Created by Maëva Mezzasalma on 02/12/2021.
+//  Copyright © 2021 Maëva Mezzasalma. All rights reserved.
+//
+
+import Foundation
+import SocketIO
+
+class SocketIOManager {
+    let socketioManager = SocketManager(socketURL: URL(string: "http://192.168.2.1:3000")!, config: SocketIOClientConfiguration(arrayLiteral: .log(false), .compress))
+    var socket: SocketIOClient
+    var isConnected = false
+    
+    static let instance = SocketIOManager()
+    
+    init() {
+        socket = socketioManager.defaultSocket
+        
+        socket.on(clientEvent: .connect) {data, ack in
+            print("socket connected yay")
+            self.socket.emit("hello", with: [["device": "iPhone"]])
+            self.isConnected = true
+        }
+        socket.on(clientEvent: .disconnect) { data, ack in
+            print("socket disconnected arrrrrgh")
+            self.isConnected = false
+        }
+    }
+    
+    func connect() {
+        socket.connect()
+    }
+    
+    func disconnect() {
+        socket.disconnect()
+    }
+    
+    func emit(event: String) {
+        if !isConnected { return }
+        socket.emit(event)
+    }
+    
+    func emit(event: String, data: Any) {
+        if !isConnected { return }
+        socket.emit(event, with: [data])
+    }
+}
+
--- GrapesApp/ViewController.swift
+++ GrapesApp/ViewController.swift
@@ -9,11 +9,122 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var socketIOconnectionLabel: UILabel!
+    @IBOutlet weak var spherosConnectionLabel: UILabel!
+    @IBOutlet weak var makeWineLabel: UILabel!
+    
+    var socketIO = SocketIOManager.instance
+    
+    var isRecording = false
+    var lastTime:Double = 0.00
+    var winemakerIsDoing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        socketIO.connect()
+        socketIOconnectionLabel.text = socketIO.isConnected ? "SocketIO connected" : "SocketIO disconnected"
+
+        socketIO.socket.on("grow", callback: { data, ack in
+            self.grapesChanging(color: .green, duration: nil)
+        })
+        socketIO.socket.on("solar", callback: { data, ack in
+            let duration = data.count > 0 ? data[0] as? Double : nil
+            self.grapesChanging(color: .purple, duration: duration)
+        })
     }
+    
+    @IBAction func socketIOconnectButton(_ sender: Any) {
+        if socketIO.isConnected {
+            print("Already connected bro'")
+        } else {
+            socketIO.connect()
+        }
+    }
+    
+    @IBAction func connectionSpheroButtonClicked(_ sender: Any) {
+        // SB-313C - SB-A729
+        SharedToyBox.instance.searchForBoltsNamed(["SB-A729", "SB-313C"]) { err in
+            if err == nil {
+                self.spherosConnectionLabel.text = "Spheros Connected"
+                
+                if let bolt = SharedToyBox.instance.bolt {
+                    bolt.sensorControl.enable(sensors: SensorMask.init(arrayLiteral: .accelerometer,.gyro))
+                    bolt.sensorControl.interval = 1
+                    bolt.setStabilization(state: SetStabilization.State.off)
+                    bolt.sensorControl.onDataReady = { data in
+                        DispatchQueue.main.async {
+                            self.onData(data: data)
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func growSenderClicked(_ sender: Any) {
+        socketIO.emit(event: "leds")
+    }
+    
+    @IBAction func makeWineClicked(_ sender: Any) {
+        self.isRecording = !self.isRecording
+        makeWineLabel.text = self.isRecording ? "Make wine started" : "Make wine stopped"
+    }
+    
+    func grapesChanging(color: UIColor, duration: Double?) {
+        var timing:Double = 60/64
+        print(["timing", timing])
+        if let duration = duration {
+            timing = duration/64 // = droneDuration/64
+        }
+        var count = 0
+        
+        for y in 0...7 {
+            for x in 0...7 {
+                let sum = timing * Double(count)
+                print([timing, count, sum])
+                DispatchQueue.main.asyncAfter(deadline: .now() + timing * Double(count)) {
+                    SharedToyBox.instance.bolts.forEach{ bolt in
+                        bolt.drawMatrix(pixel: Pixel(x: x, y: y), color: color)
+                    }
+                }
+                count+=1
+            }
+        }
+    }
+        
+    func onData(data: SensorControlData) {
+        if self.isRecording {
+            print("recording")
+            if let acceleration = data.accelerometer?.filteredAcceleration {
 
-
+                if let z = acceleration.z,
+                   let y = acceleration.y,
+                   let x = acceleration.x {
+                    
+                    let absSum = abs(x)+abs(y)+abs(z)
+                    
+                    if (absSum >= 2) {
+                        if(!winemakerIsDoing) {
+                            winemakerIsDoing = true
+                            self.socketIO.emit(event: "winemaker", data: 1)
+                            print("changed, 1")
+                        }
+                        lastTime = NSDate.timeIntervalSinceReferenceDate
+                    }
+                    
+                    if(NSDate.timeIntervalSinceReferenceDate - lastTime > 0.5) {
+                        if(winemakerIsDoing) {
+                            winemakerIsDoing = false
+                            self.socketIO.emit(event: "winemaker", data: 0)
+                            print("changed, 0")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
